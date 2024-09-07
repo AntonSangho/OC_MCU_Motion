ee1764186a41fe3573bc12fb39c94cf7dcbec5dd
AntonSangho <sanghoemail@gmail.com>
2024-09-08 00:07:22 +0900

    add fileter code

---
+++ src/filter/filter.ino
@@ -0,0 +1,19 @@
+
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	int sensorValue = analogRead(A0);
+	float filteredValue =0;
+	for (int i = 0; i < 100; i++) {
+		filteredValue += analogRead(A0);
+		delayMicroseconds(100);
+	}
+	filteredValue /= 100;
+
+	Serial.print(sensorValue);
+	Serial.print(",");
+	Serial.println(filteredValue);
+	delay(100);
+}
